d27e143f1050bb9af081843848a62d255d85095c
Larisasmiles <larisasmiles@gmail.com>
2017-07-23 16:32:35 -0600

    adding notes

--- lib/hyde.rb
+++ lib/hyde.rb
@@ -1,2 +1,21 @@
 class Hyde
+  function newsite() {
+  mkcd "$*"  # mkdir and cd into it
+  mkdir "js"
+  mkdir "imgs"
+  cat > index.html <<'EOI'
+<html>
+<head>
+</head>
+<body>
+</body>
+</html>
+EOI
+  cat > main.css <<'EOI'
+body {
+ font-family: Arial;
+}
+EOI
+  vim "index.html"
+}
 end
