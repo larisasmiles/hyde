25004da008bd399276877ad677206538a7968c5c
Larisasmiles <larisasmiles@gmail.com>
2017-07-25 14:23:38 -0600

    made some new methods to create tree

--- lib/hyde.rb
+++ lib/hyde.rb
@@ -1,29 +1,42 @@
+require 'fileutils'
+require "pry"
+
 class Hyde
   attr_reader :user_cmd, :user_cmd2
 
   def initialize(argv)
     @user_cmd = argv[0]
     @user_cmd2 = argv[1]
-    @sub_directories = sub_directories
+
   end
 
   def create_dir
     return false if Dir.exist?(File.join(Dir.home, "#{user_cmd2}"))
-    Dir.mkdir(File.join(Dir.home, "#{user_cmd2}"))
+    Dir.mkdir("#{user_cmd2}")
+    puts "string"
   end
 
   def evaluate
     if user_cmd == "new"
       create_dir
-      #retMessage = "Directory already exists!"
+      directory_tree
+      # file_tree
     else
       return
     end
   end
 
-  def sub_directories
-    ["_output", "source/"]
+  def directory_tree
 
+    Dir.mkdir("#{user_cmd2 + "/_output"}")
+    # Dir.mkdir("#{user_cmd2}" + "/source")
+    # Dir.mkdir("#{user_cmd2}" + "/source/css")
+    # Dir.mkdir("#{user_cmd2}" + "/source/pages")
+    # Dir.mkdir("#{user_cmd2}" + "/source/posts")
+  end
+
+  def file_tree
+    touch user_cmd2 + "/source/index.markdown"
+    touch user_cmd2 + "/source/main.css"
   end
 end
-# make directory structure a data structure to be able to iterate through
--- test/hyde_test.rb
+++ test/hyde_test.rb
@@ -5,10 +5,11 @@ class HydeTest < Minitest::Test
 
   def test_if_error_if_no_argv_1
     h = Hyde.new(["new", ""])
-    retVal = h.evaluate
-
+    retVal = h.evaluate()
     assert_equal retVal, false
   end
 
-  
+  def test_it_creates_directory_tree
+    
+
 end
